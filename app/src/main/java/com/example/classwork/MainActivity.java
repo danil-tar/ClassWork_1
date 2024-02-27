e2e52c9ece0a2c31b3d73fb77a7a9142d5bef784
Dan <danilviktar@gmail.com>
2024-02-27 22:45:04 +0200

    variant using reflection ClassWork_1

--- app/src/main/java/com/example/classwork/MainActivity.java
+++ app/src/main/java/com/example/classwork/MainActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.radiobutton.MaterialRadioButton;
+import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void changeColorTextButton(View view) {
+    public void changeColorText(View view) {
+        Object o = view;
+        Class<?> aClass = o.getClass();
+        Log.e("GET", "GET CLASS VIEW:  " + aClass);
+
+        if (view.isClickable()) {
+            if (aClass == MaterialRadioButton.class) {
+                changeColorToButton(view);
+
+            } else if (aClass == MaterialTextView.class) {
+                changeColorToTextView(view);
+            }
+        }
+    }
+
+    private static void changeColorToTextView(View view) {
+        TextView textView = (TextView) view;
+        if (textView.isClickable()) {
+            textView.setTextColor(Color.CYAN);
+        }
+    }
+
+    private static void changeColorToButton(View view) {
         Button button = (Button) view;
         if (button.isClickable()) {
             int viewId = view.getId();
